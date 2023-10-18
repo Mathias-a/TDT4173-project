04768f01bedefc3ea206abfb69e52dc4cd800bf3
Mathias Aas <mathias.aas123@gmail.com>
2023-10-18 16:56:36 +0200

    add fields fields for hour and month

--- solar_predictor.py
+++ solar_predictor.py
@@ -5,7 +5,6 @@ import torch.optim as optim
 import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 # %% [markdown]
@@ -13,6 +12,14 @@ from sklearn.preprocessing import StandardScaler
 # 
 
 # %%
+
+CUSTOM_COLUMNS_TO_KEEP = [
+    "hour_cos",
+    "month_sin",
+    "hour_sin",
+    "month_cos",
+]
+
 COLUMNS_TO_KEEP = [
     "direct_rad:W",
     "clear_sky_rad:W",
@@ -23,16 +30,16 @@ COLUMNS_TO_KEEP = [
     "diffuse_rad_1h:J",
     "is_day:idx",
     "sun_elevation:d",
-    "ceiling_height_agl:m",
-    "effective_cloud_cover:p",
-    "visibility:m",
-    'total_cloud_cover:p',
-    'air_density_2m:kgm3',
-    'wind_speed_v_10m:ms',
-    'dew_point_2m:K',
-    'wind_speed_u_10m:ms',
-    't_1000hPa:K',
-    'absolute_humidity_2m:gm3',
+    # "ceiling_height_agl:m",
+    # "effective_cloud_cover:p",
+    # "visibility:m",
+    # 'total_cloud_cover:p',
+    # 'air_density_2m:kgm3',
+    # 'wind_speed_v_10m:ms',
+    # 'dew_point_2m:K',
+    # 'wind_speed_u_10m:ms',
+    # 't_1000hPa:K',
+    # 'absolute_humidity_2m:gm3',
     # 'snow_water:kgm2',
     # 'relative_humidity_1000hPa:p',
     # 'fresh_snow_24h:cm',
@@ -59,16 +66,18 @@ COLUMNS_TO_KEEP = [
     # 'snow_drift:idx',
     # 'snow_melt_10min:mm',
     # 'wind_speed_w_1000hPa:ms',
-    # "date_calc", something wrong with this column
+    # "date_calc", 
     "pv_measurement",
-]
-LEARNING_RATE = 0.001
+] + CUSTOM_COLUMNS_TO_KEEP
+
+LEARNING_RATE = 0.00008
 NUM_EPOCHS = 100
 BATCH_SIZE = 32
-NUM_FEATURES = len(COLUMNS_TO_KEEP) - 1  # -1 because pv_measurement is the target
+NUM_FEATURES = len(COLUMNS_TO_KEEP) - 1  # -1 because pv_measurement is the target, +4 FOR HOUR
 FEATURE_SIZE = 4  # 7 days of hourly data
-WEIGHT_DECAY = 0.01
-SEQUENCE_LENGTH = 14*24
+WEIGHT_DECAY = 0.12
+SEQUENCE_LENGTH = 24
+LOCATION = "C"
 
 # %% [markdown]
 # # Neural net
@@ -86,6 +95,15 @@ def create_sequences(data, sequence_length):
         sequences.append(seq)
     return np.array(sequences)
 
+def apply_pca(data, q=None, center=True, niter=2):
+    """
+    Applies PCA to the given data tensor.
+    Returns transformed data and PCA components.
+    """
+    U, S, V = torch.pca_lowrank(data, q=q, center=center, niter=niter)
+    transformed_data = torch.matmul(data, V)
+    return transformed_data, V
+
 
 class SolarPredictionNet(nn.Module):
     def __init__(self, sequence_length, num_channels):
@@ -93,6 +111,7 @@ class SolarPredictionNet(nn.Module):
 
         self.conv1 = nn.Conv1d(num_channels, 32, kernel_size=sequence_length, stride=1)
         self.conv2 = nn.Conv1d(32, 64, kernel_size=1, stride=1)
+        self.dropout = nn.Dropout(0.5)  # Add dropout layer
         self.fc1 = nn.Linear(64, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 1)
@@ -103,61 +122,92 @@ class SolarPredictionNet(nn.Module):
         x = self.relu(self.conv2(x))
         x = x.view(x.size(0), -1)  # Flatten
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)  # Use dropout after first fully connected layer
         x = self.relu(self.fc2(x))
         return self.fc3(x)
+    
 
 # %% [markdown]
-# # Load dataset
-# 
+# # Load and Preprocess Data
 
 # %%
-# Load data from Parquet files
-location = "A"
-df_data = pd.read_parquet(f"data/{location}/X_train_observed.parquet")
-df_target = pd.read_parquet(f"data/{location}/train_targets.parquet")
+# 1. Load data
+df_observed = pd.read_parquet(f"data/{LOCATION}/X_train_observed.parquet")
+df_estimated = pd.read_parquet(f"data/{LOCATION}/X_train_estimated.parquet")
+df_target = pd.read_parquet(f"data/{LOCATION}/train_targets.parquet")
 
-# Merge the datasets
+# 2. Combine observed and estimated datasets
+df_combined = pd.concat([df_observed, df_estimated], axis=0).sort_values(by='date_forecast')
+
+# 3. Merge with target data
 df_merged = pd.merge(
-    df_data, df_target, left_on="date_forecast", right_on="time", how="inner"
+    df_combined, df_target, left_on="date_forecast", right_on="time", how="inner"
 )
 
+# %% [markdown]
+# # Downsampling and Feature Engineering
+
+# %%
 # Downsampling the dataframe to hourly intervals
 df_merged = df_merged.resample('H', on="date_forecast").mean()
 
+# Add columns for hour of day, and month of year using sine and cosine to capture the cyclical nature
+df_merged['hour_sin'] = np.sin(2 * np.pi * df_merged.index.hour / 24)
+df_merged['hour_cos'] = np.cos(2 * np.pi * df_merged.index.hour / 24)
+
+df_merged['month_sin'] = np.sin(2 * np.pi * df_merged.index.month / 12)
+df_merged['month_cos'] = np.cos(2 * np.pi * df_merged.index.month / 12)
+
+# Keep only relevant columns
 df_merged = df_merged[COLUMNS_TO_KEEP]
 
-# Set all NaN values to 0
-df_merged.fillna(0, inplace=True)
-
-
-
+# 4. Extract features and target
+df_merged.fillna(0, inplace=True)  # Fill NaN values
 y = df_merged["pv_measurement"]
 X = df_merged.drop("pv_measurement", axis=1)
 
+# %% [markdown]
+# # Data Preparation for Modeling
+
+# %%
 # Convert dataframes to sequences
 X_sequences = create_sequences(X.values, SEQUENCE_LENGTH)
-# Adjust the sequence creation for y
-y_sequences = y.values[SEQUENCE_LENGTH-1:-1]  # Aligned with the end of each sequence and remove the last element
+y_sequences = y.values[SEQUENCE_LENGTH-1:]  # Aligned with the end of each sequence
 
 # Sequential Split
 train_size = int(0.8 * len(X_sequences))
 X_train, X_val = X_sequences[:train_size], X_sequences[train_size:]
 y_train, y_val = y_sequences[:train_size], y_sequences[train_size:]
 
-# Normalize the data
+# Normalize the data and apply PCA
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
 X_val = scaler.transform(X_val.reshape(-1, X_val.shape[-1])).reshape(X_val.shape)
 
+# Convert to tensors and apply PCA
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+X_val_tensor = torch.tensor(X_val, dtype=torch.float32)
+
+X_train_tensor, pca_components = apply_pca(X_train_tensor, q=NUM_FEATURES)
+X_val_tensor, _ = apply_pca(X_val_tensor, q=NUM_FEATURES)
+
+# Ensure shapes are [batch, channels, sequence]
+X_train_tensor = X_train_tensor.transpose(1, 2)  
+X_val_tensor = X_val_tensor.transpose(1, 2)
+
+# Update NUM_FEATURES to the size after PCA
+NUM_FEATURES = pca_components.shape[1]
 
 # Convert data to PyTorch tensors
-X_train_tensor = torch.tensor(X_train, dtype=torch.float32).transpose(1, 2)  # Adjust shape to [batch, channels, sequence]
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32).transpose(1, 2)
 y_train_tensor = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
-X_val_tensor = torch.tensor(X_val, dtype=torch.float32).transpose(1, 2)  # Adjust shape to [batch, channels, sequence]
+X_val_tensor = torch.tensor(X_val, dtype=torch.float32).transpose(1, 2)
 y_val_tensor = torch.tensor(y_val, dtype=torch.float32).unsqueeze(1)
 
+# %% [markdown]
+# # Dataset and DataLoader Preparation
 
-# Create a custom dataset
+# %%
 class SolarDataset(Dataset):
     def __init__(self, X, y):
         self.X = X
@@ -169,7 +219,6 @@ class SolarDataset(Dataset):
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
 
-
 # Create datasets for training and validation
 train_dataset = SolarDataset(X_train_tensor, y_train_tensor)
 val_dataset = SolarDataset(X_val_tensor, y_val_tensor)
@@ -178,48 +227,31 @@ val_dataset = SolarDataset(X_val_tensor, y_val_tensor)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-# ... [Neural Network and Training code from previous messages]
-
-
 # %% [markdown]
 # # Training Loop
 # 
 
 # %%
-import matplotlib.pyplot as plt
-def plot_losses(losses):
-    # Create an array to represent the number of epochs or iterations
-    # This assumes that you have the same number of losses as epochs or iterations
-    epochs = range(1, len(losses) + 1)
-
-    # Create the plot
-    plt.figure(figsize=(8, 6))  # Adjust the figure size as needed
-    plt.plot(epochs, losses, label='Validation Loss', marker='o', linestyle='-', color='b')
-    plt.title('Validation Loss Over Epochs')
-    plt.xlabel('Epochs')
-    plt.ylabel('Validation Loss')
-    plt.legend()
-    plt.grid(True)
-
-    # Display the plot (if you're using Jupyter Notebook, this step might be optional)
-    plt.show()
-
-# %%
 def train_model(model, train_loader, val_loader):
     criterion = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
-    val_losses = []
 
     for epoch in range(NUM_EPOCHS):
         model.train()
+
+        total_train_loss = 0.0  # Initialize accumulated training loss
+
         for batch_idx, (data, target) in enumerate(train_loader):
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output, target)
+            total_train_loss += loss.item()  # Accumulate the training loss
             loss.backward()
             optimizer.step()
 
-        # Evaluate the model on the validation set
+        avg_train_loss = total_train_loss / len(train_loader)  # Compute the average training loss
+
+        # Evaluate the model on the validation set (This part remains unchanged)
         model.eval()
         val_loss = 0.0
         with torch.no_grad():
@@ -227,18 +259,15 @@ def train_model(model, train_loader, val_loader):
                 output = model(data)
                 loss = criterion(output, target)
                 val_loss += loss.item()
-                
 
-        # Average validation loss
-        val_loss /= len(val_loader)
-        val_losses.append(val_loss)
+        avg_val_loss = val_loss / len(val_loader)  # Compute the average validation loss
 
         print(
-            f"Epoch {epoch + 1}/{NUM_EPOCHS}, Training Loss: {loss.item()}, Validation Loss: {val_loss}"
+            f"Epoch {epoch + 1}/{NUM_EPOCHS}, Training Loss: {avg_train_loss}, Validation Loss: {avg_val_loss}"
         )
-        plot_losses(val_losses)
 
     print("Training complete!")
+
 
 
 # %%
@@ -251,9 +280,7 @@ def save_model(model, location):
 
 model = SolarPredictionNet(SEQUENCE_LENGTH, NUM_FEATURES)
 train_model(model, train_loader, val_loader)
-save_model(model, location) 
-
-
+save_model(model, LOCATION) 
 
 # %%
 
@@ -281,25 +308,27 @@ def make_predictions(location, df_test):
     # Set the date_calc column as the index for resampling
     df_test.set_index('date_forecast', inplace=True)
     # Resample to 1-hour intervals
-
     df_test = df_test.resample('1H').mean()
     df_test = df_test.dropna(how='all').reset_index(drop=True)
     
-    # Drop columns that are not required
-    df_test = df_test.drop(['date_forecast', 'date_calc'], axis=1, errors='ignore')
+    # Keep only the columns used during training (minus the target column)
+    relevant_columns = [col for col in COLUMNS_TO_KEEP if col != "pv_measurement"]
+    df_test = df_test[relevant_columns]
+    
     # Fill NaNs (if any after resampling)
     df_test.fillna(0, inplace=True)
     # Create sequences and normalize
     padded_data = pad_data(df_test.values, SEQUENCE_LENGTH)
     test_sequences = create_sequences(padded_data, SEQUENCE_LENGTH)
-    print(len(test_sequences))
     test_sequences = scaler.transform(test_sequences.reshape(-1, test_sequences.shape[-1])).reshape(test_sequences.shape)
     test_tensor = torch.tensor(test_sequences, dtype=torch.float32).transpose(1, 2)
+    
     # Make predictions
     with torch.no_grad():
         predictions = model(test_tensor)
         predictions = predictions.numpy().flatten()
     return predictions
+
 
 
 # Read the Kaggle test.csv to get the location and ids
@@ -309,6 +338,7 @@ locations = ["A", "B", "C"]
 
 # Iterate over the locations and fill in the predictions
 for loc in locations:
+    print(loc)
     # Load forecasted weather data for testing for the current location
     df_loc = pd.read_parquet(f"data/{loc}/X_test_estimated.parquet")
     preds = make_predictions(loc, df_loc)
