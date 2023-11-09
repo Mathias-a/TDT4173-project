b6ea971e1aae311be2d35914de69a4a9c06ddb56
Simen Seeberg-Rommetveit <simensee@gmail.com>
2023-11-09 23:13:54 +0100

    alot

--- absolute_value.py
+++ absolute_value.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('median_ensemble_predictions.csv')
+df = pd.read_csv('combined_predictions_A_ensembled_BC_specialized.csv')
+# df = pd.read_csv('kaggle_submission_catboost_13.csv')
 
 # Ensure there are no negative predictions
 df['prediction'] = df['prediction'].apply(lambda x: max(x, 0))
 
-df['prediction'] = df['prediction'].apply(lambda x: 0 if x < 1 else x)
+df['prediction'] = df['prediction'].apply(lambda x: 0 if x < 0.1 else x)
 
 
 # Set predictions to zero where up to three non-zero predictions are surrounded by zeros
@@ -21,6 +22,7 @@ for i in range(1, len(df) - 1):
     # Check three consecutive non-zero predictions surrounded by zeros
 
 # Save the updated DataFrame back to a CSV file
-df.to_csv('median_ensemble_predictions_updated.csv', index=False)
+# df.to_csv('predictions_updated_13.csv', index=False)
+df.to_csv('combined_predictions_A_ensembled_BC_specialized.csv', index=False)
 
 print("Single, double, or triple non-zero predictions surrounded by zeros have been set to zero.")
--- average_ensemble_all_locations.py
+++ average_ensemble_all_locations.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 df1 = pd.read_csv('predictions_updated_150.csv')
-df2 = pd.read_csv('predictions_updated_7.csv')
+df2 = pd.read_csv('predictions_updated_10.csv')
+df3 = pd.read_csv('predictions_updated_11.csv')
 
 # Verify that the IDs match in both DataFrames (assuming they are sorted and have the same length)
-if not df1['id'].equals(df2['id']):
-    raise ValueError("The IDs in both CSV files do not match.")
+
 
 # Calculate the average of the predictions for all rows
 df_average_all = pd.DataFrame({
     'id': df1['id'],
-    'prediction': (df1['prediction'] + df2['prediction']) / 2
+    'prediction': (df1['prediction']*0.25 + df2['prediction']*0.5 + df3['prediction']*0.25) 
 })
 
 # Save the averaged predictions to a new CSV file
-df_average_all.to_csv('averaged_predictions_all_7_150.csv', index=False)
+df_average_all.to_csv('averaged_predictions_three best.csv', index=False)
