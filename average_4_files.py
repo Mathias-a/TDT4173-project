f97f627c70e920bf8d9242f42433d1547ba24e61
Simen Seeberg-Rommetveit <simensee@gmail.com>
2023-11-12 18:58:59 +0100

    ensemble

--- average_4_files.py
+++ average_4_files.py
@@ -1,20 +1,18 @@
 import pandas as pd
 
 # Load the datasets
-df1 = pd.read_csv('predictions_updated_7.csv')
-df2 = pd.read_csv('predictions_updated_8.csv')
-df3 = pd.read_csv('predictions_updated_150.csv')
-df4 = pd.read_csv('predictions_updated_155.csv')
-
+df1 = pd.read_csv('predictions/autogluon-Erik.csv')
+df2 = pd.read_csv('predictions/autogluon-2-stack-params.csv')
+df3 = pd.read_csv('predictions/predictions_updated_10.csv')
 # Verify that the IDs match in all DataFrames (assuming they are sorted and have the same length)
-if not (df1['id'].equals(df2['id']) and df1['id'].equals(df3['id']) and df1['id'].equals(df4['id'])):
-    raise ValueError("The IDs in the CSV files do not match.")
+# if not (df1['id'].equals(df2['id']) and df1['id'].equals(df3['id']) and df1['id']):
+#     raise ValueError("The IDs in the CSV files do not match.")
 
 # Calculate the average of the predictions for all rows
 df_average = pd.DataFrame({
     'id': df1['id'],
-    'prediction_average': (df1['prediction'] + df2['prediction'] + df3['prediction'] + df4['prediction']) / 4
+    'prediction': (df1['prediction'] + df3['prediction'] + df2['prediction'])/3
 })
 
 # Save the averaged predictions to a new CSV file
-df_average.to_csv('averaged_predictions.csv', index=False)
+df_average.to_csv('predictions/averaged_autogluon_erik_145_mathias.csv', index=False)
